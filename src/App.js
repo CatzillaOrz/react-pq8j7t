c631726f224c410965108e8d15e3beba1efbeff2
CatzillaOrz <catzillaorz@outlook.com>
2022-11-17 16:26:35 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import "./style.css";
 export default function App() {
   const Greeting = {
     hello: 'Hello StackBlitz!',
-    desrc: 'Start editing to see some magic happen :)'
+    desrc: 'Start editing to see some magic happen :)',
+    enable: true
   }
   return (
     <div>
       <h1>{Greeting.hello}</h1>
-      <p>{Greeting.desrc}</p>
+      {Greeting.enable && <p>{Greeting.desrc}</p>}
     </div>
   );
 }
