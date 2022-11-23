d9eced24ae5741f7fe65d6881677837867bad8a7
catzilla <catzillaorz@outlook.com>
2022-11-23 21:17:56 +0800

    feat: ract-router-dom | webpack set historyApiFallback: true

--- src/app.js
+++ src/app.js
@@ -1,7 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+const CreatePage = () => (
+  <div>
+    This is from my createPage
+    <Link to="/">go back</Link>
+  </div>
+);
+
+const Edit = () => (
+  <div>
+    This is from my EditPage
+    <Link to="/">go back</Link>
+  </div>
+);
+
+const App = () => <div>Interesting</div>;
+
+const NotFound = () => (
+  <div>
+    not found! <Link to="/">Go back</Link>
+  </div>
+);
+
+const Help = () => (
+  <div>
+    no help! <Link to="/">Go back</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink activeClassName="is-active" to="/create">
+      Create
+    </NavLink>
+    <NavLink activeClassName="is-active" to="/edit">
+      Edit
+    </NavLink>
+    <NavLink activeClassName="is-active" to="/help">
+      Help
+    </NavLink>
+  </header>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <div>
+        <Header />
+        <Route path="/" component={App} exact={true} />
+        <Route path="/create" component={CreatePage} />
+        <Route path="/edit" component={Edit} />
+        <Route path="/help" component={Help} />
+        <Route path="*" component={NotFound} />
+      </div>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,6 @@ module.exports = {
   devtool: "eval-cheap-module-source-map",
   devServer: {
     static: path.join(__dirname, "public"),
+    historyApiFallback: true,
   },
 };
