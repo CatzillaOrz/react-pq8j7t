0c8944f069085390b8e76ddaab2f51f6d0817836
catzilla <catzillaorz@outlook.com>
2022-11-30 00:03:40 +0800

    feat: webpack build:dev  | build:prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    static: path.join(__dirname, "public"),
-    historyApiFallback: true,
-  },
+module.exports = (env) => {
+  const isProduction = env["production"] === "production";
+  console.log(env);
+  console.log(env["production"]);
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    mode: isProduction ? "production" : "development",
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "eval-cheap-module-source-map",
+    devServer: {
+      static: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
