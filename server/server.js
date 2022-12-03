0a827f27d56d48e17e14246044d60d3c3ca0e2b8
catzilla <catzillaorz@outlook.com>
2022-12-03 15:46:33 +0800

    feat: setup express get router req.sendFile(_path,index.html) to handle reload refresh html

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const publicPath = path.join(__dirname, "..", "public");
 
 app.use(express.static(publicPath));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
 app.listen(3000, (_) => {
   console.log("server is running on port 3000");
 });
