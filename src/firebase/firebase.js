0d2f92e2641a9a7433026e79e718cae22476c89d
catzilla <catzillaorz@outlook.com>
2022-12-07 16:20:33 +0800

    feat: firebase | create get data using firebase SDK

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,43 @@
+import uuid from "uuid";
 import { initializeApp } from "firebase/app";
-import * as expenssActions from "../actions/expenses";
+import { child, getDatabase, ref, set, get } from "firebase/database";
 
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyAdxG8rbixjz8QA7FZLB4FyJ21x9umaGBk",
-  authDomain: "expensify-5ad85.firebaseapp.com",
-  projectId: "expensify-5ad85",
-  storageBucket: "expensify-5ad85.appspot.com",
-  messagingSenderId: "845594625540",
-  appId: "1:845594625540:web:f0a49634caf779c1ea9751",
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://expensify-5ad85-default-rtdb.firebaseio.com",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+// Initialize Realtime Database and get a reference to the service
+//const database = getDatabase(app);
+
+function writeUserData(userId, name, email, imageUrl) {
+  set(ref(db, "users/" + userId), {
+    username: name,
+    email: email,
+    profile_picture: imageUrl,
+  });
+}
+
+//writeUserData(uuid(), "Tom", "tom@gmail.com", "/public/images/avatar.jpeg");
+//
+
+const dbRef = ref(db);
+const userId = "3c2872c1-c9ef-40fd-a9e2-85f3c2d25f50";
+get(child(dbRef, `users/${userId}`))
+  .then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
