106f37a852b2beaa3ee9f6648277e76a9e8b36f2
catzilla <catzillaorz@outlook.com>
2022-12-20 00:48:16 +0800

    feat: Public Only | Private Only Routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,11 @@ import { startLogout } from "../actions/auth";
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink activeClassName="is-active" to="/">
+    <NavLink activeClassName="is-active" to="/dashboard">
       Dashboard
     </NavLink>
     <NavLink activeClassName="is-active" to="/create">
       Create
-    </NavLink>
-    <NavLink activeClassName="is-active" to="/edit">
-      Edit
     </NavLink>
     <NavLink activeClassName="is-active" to="/help">
       Help
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,24 +6,28 @@ import ExpenseDashboardPage from "../components/ExpenseDashboard";
 import EditExpensePage from "../components/EditExpensePage";
 import HelpPage from "../components/HelpPage";
 import NotFoundPage from "../components/NotFoundPage";
-import Header from "../components/Header";
 import LoginPage from "../components/LoginPage";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 
 export const history = createHistory();
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route path="/" component={LoginPage} exact={true} />
-        <Route
+        <PublicRoute path="/" component={LoginPage} exact={true} />
+        <PrivateRoute
           path="/dashboard"
           component={ExpenseDashboardPage}
           exact={true}
         />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} exact={true} />
+        <PrivateRoute path="/create" component={AddExpensePage} />
+        <PrivateRoute
+          path="/edit/:id"
+          component={EditExpensePage}
+          exact={true}
+        />
         <Route path="/help" component={HelpPage} />
         <Route path="*" component={NotFoundPage} />
       </Switch>
